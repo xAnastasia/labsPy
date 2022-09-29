2810f28bbc652c81b76fcb6dc2445f1745465cab
xAnastasia <114685647+xAnastasia@users.noreply.github.com>
2022-09-29 23:35:08 +0700

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,18 @@
+#Первое и второе задание
+seconds_per_min = 60
+seconds_per_hour = seconds_per_min * seconds_per_min
+print("Количество секунд в часе: " + str(seconds_per_hour))
+
+#Третье задание
+hour_per_day = 24
+seconds_per_day = seconds_per_hour * hour_per_day
+print("Количество секунд в сутках: " + str(seconds_per_day))
+
+#Четвертое задание
+result1 = seconds_per_day / seconds_per_hour
+print("Результат 1: " + str(result1))
+
+#Пятое задание
+result2 = seconds_per_day // seconds_per_hour
+print("Результат 2: " + str(result2))
+
